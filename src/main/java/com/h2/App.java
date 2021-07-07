44feb6434cee190d3171590243349d8a36bbfadf
yhangliu <37546201+yhangliu@users.noreply.github.com>
2021-07-07 15:42:43 -0400

    part 1 check

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,15 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
     }
+    
+    public static int add(int[] numbers) {
+    	int sum = 0;
+    	for (int i = 0; i <= numbers.length; i++) {
+    		sum = sum + numbers[i];
+    	}
+        return sum;
+    }
+    
 }
