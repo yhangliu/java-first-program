99879caefca75a3f9f602d2432d7e8a90b1c0adb
yhangliu <37546201+yhangliu@users.noreply.github.com>
2021-07-07 15:43:37 -0400

    made add private

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -15,7 +15,7 @@ public class App
         return 2 * number;
     }
     
-    public static int add(int[] numbers) {
+    private static int add(int[] numbers) {
     	int sum = 0;
     	for (int i = 0; i <= numbers.length; i++) {
     		sum = sum + numbers[i];
