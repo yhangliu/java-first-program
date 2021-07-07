6ca0508f747cb7092156256506d8d5ef169c2627
yhangliu <37546201+yhangliu@users.noreply.github.com>
2021-07-07 16:08:08 -0400

    2.3 -2.6

--- src/test/java/com/h2/SavingsCalculator.java
+++ src/test/java/com/h2/SavingsCalculator.java
@@ -2,5 +2,16 @@ package com.h2;
 
 public class SavingsCalculator {
 	private float[] credits,debits;
+	public SavingsCalculator(float[] credits, float[] debits){
+		this.credits = credits;
+		this.debits = debits;
+	}
+	private float sumOfCredits() {
+		float sum = 0.0f;
+		for(int i = 0;i < this.credits.length ;i++) {
+			sum = sum + this.credits[i];
+		}
+		return sum;
+	}
 
 }
