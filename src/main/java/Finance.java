b09efbad48f2e71d721153c308979c4801da2e69
yhangliu <37546201+yhangliu@users.noreply.github.com>
2021-07-08 18:28:28 -0400

    Update Finance.java

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,8 +1,6 @@
 import java.util.Map;
 
-import com.h2.BestLoanRates;
-import com.h2.MortgageCalculator;
-import com.h2.SavingsCalculator;
+
 
 public class Finance {
 	
@@ -29,22 +27,5 @@ public class Finance {
 
 	}
 	
-	private static void executeCommand(String command, String[] arguments) {
-		
-		switch(command) {
-		case BEST_LOAN_RATES:
-			System.out.println("Finding best loan rates ...");
-			BestLoanRates.main(arguments);
-			return;
-		case SAVINGS_CALCULATOR:
-			System.out.println("Finding your net savings ...");
-			SavingsCalculator.main(arguments);
-			return;
-		case BEST_LOAN_RATES:
-			System.out.println("Finding your monthly payment ...");
-			MortgageCalculator.main(arguments);
-			return;
-		}
-		
-	}
+
 }
