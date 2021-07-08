1915222d4280ba20737c44817426122045e94590
yhangliu <37546201+yhangliu@users.noreply.github.com>
2021-07-08 17:08:32 -0400

    4.5

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -9,5 +9,14 @@ public class Finance {
 	public final static Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
 			SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
 			MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
+		
+	private static boolean validateCommandArguments(String[] args) {
+		switch (args.length) {
+		case 1: return true;
+		case 3: return true;
+		case 4: return true;
+		}
 
+		return false;
+	}
 }
